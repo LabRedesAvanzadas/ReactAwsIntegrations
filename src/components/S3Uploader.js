1f933e63d5f97ef3ec82de60df45baeefdcfda19
CarlosCedeniio <caencede@espol.edu.ec>
2023-07-03 20:16:22 -0500

    Update S3Uploader.js

--- src/components/S3Uploader.js
+++ src/components/S3Uploader.js
@@ -33,7 +33,7 @@ export const S3Uploader = () => {
       }
       return (
         <div style={{ marginTop: '150px' }}>
-          <h1 style={{color: "#a90311"}}>Subida de archivo simpleROJOOaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaOO</h1>
+          <h1 style={{color: "#a90311"}}>Ultimo dia de clases oficiales :c</h1>
           <input type="file" onChange={handleFileSelect} />
           {file && (
             <div style={{ marginTop: '10px' }}>
